b53eff0c1df8511ef3efe52ae37061cea9d8819c
ellocoPrueba <pipoquintagomezr@gmail.com>
2024-03-28 23:01:48 -0500

    make it better

--- guardar.php
+++ guardar.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video y fotos</title>
+    <link rel="stylesheet" href="guardar.css">
+</head>
+<body>
 <?php
 
 require 'conexion.php';
@@ -10,7 +19,19 @@ $insertar = "INSERT INTO usu VALUES ('$usuario','$password')";
 $query = mysqli_query($conectar,$insertar);
 
 if($query){
-    echo "LA PAGINA ESTA EN MANTENIMIENTO POR FAVOR MIL DISCULPAS";
+    echo "
+    <p>
+    LA PAGINA ESTA EN MANTENIMIENTO 
+    <br />
+    <br />
+    PODRA VER Y DESCARGAR LOS VIDEOS Y FOTOS LUEGO
+    <br />
+    <br />
+    DISCULPE LA MOLESTIA 
+    <p>
+    ";
 }
 
 ?>
+</body>
+</html>
